e641aba96cfa3d1c31247cd7d9d810c5b6223803
a-kieffer <akieffer@orange.fr>
2019-07-01 15:11:14 +0200

    Adding the plot for fake tracklets and titles

--- plot_efficiency_lambda_cut.C
+++ plot_efficiency_lambda_cut.C
@@ -36,7 +36,7 @@ using Vertex = o2::dataformats::Vertex<o2::dataformats::TimeStamp<int>>;
 
 void plot_efficiency_lambda_cut(){
 
-    TFile *f = new TFile("lambda_cut_variation_phi_0_05.root");
+    TFile *f = new TFile("lambda_cut_variation_phi_0_005.root");
 
     TNtuple * data ;
     f->GetObject("Results", data);
@@ -75,28 +75,35 @@ void plot_efficiency_lambda_cut(){
 
     int NumDistinctCuts=0;
     double CurrentMeanEffReco=0;
+    double CurrentMeanEffFake=0;
     double CurrentInvCut;
     int CountCurrentInvCut=0;
     std::vector<double> DistinctCuts;
     std::vector<double> MeanEffRecoCut;
-    double mean;
+    std::vector<double> MeanEffFakeCut;
+    double meanEffReco;
+    double meanEffFake;
 
     for(int i=0; i<nEntries; i++){
         if( std::find(DistinctCuts.begin(), DistinctCuts.end(), ArrInvCut[i])==DistinctCuts.end() || i==0){ //we have not processed this value
             CurrentInvCut=ArrInvCut[i];
             DistinctCuts.push_back(CurrentInvCut);
             CurrentMeanEffReco=0;
+            CurrentMeanEffFake=0;
             CountCurrentInvCut=0;
             NumDistinctCuts++;
             for(int j=i; j<nEntries; j++){
                 if(ArrInvCut[j]==CurrentInvCut){
                     CurrentMeanEffReco+=ArrEffReco[j];
+                    CurrentMeanEffFake+= ArrEffFake[j];
                     CountCurrentInvCut++;
                 }
             }
-            mean = CurrentMeanEffReco/CountCurrentInvCut;
-            std::cout<<"Cut :"<<1/CurrentInvCut<<"  mean : "<<mean<<std::endl;
-            MeanEffRecoCut.push_back(mean);
+            meanEffReco = CurrentMeanEffReco/CountCurrentInvCut;
+            meanEffFake = CurrentMeanEffFake/CountCurrentInvCut;
+            std::cout<<"Cut :"<<1/CurrentInvCut<<"  mean reco : "<<meanEffReco<<"  mean fake : "<<meanEffFake<<std::endl;
+            MeanEffRecoCut.push_back(meanEffReco);
+            MeanEffFakeCut.push_back(meanEffFake);
         }else{
             continue;
         }  
@@ -104,7 +111,7 @@ void plot_efficiency_lambda_cut(){
 
 
 
-    TCanvas * c1=new TCanvas("c1","A Zoomed Graph",1000,700);
+    TCanvas * c1=new TCanvas("c1","Efficiency Reconstruction",1000,700);
   // TH2F * hpx = new TH2F("hpx","Zoomed Graph Example",10,0,0.05,10,1,1);
    //hpx->SetStats(kFALSE);   // no statistics
    //hpx->Draw();
@@ -112,17 +119,39 @@ void plot_efficiency_lambda_cut(){
 
     c1->SetLogx();
 
-    TGraph * graph= new TGraph(nEntries, ArrInvCut,ArrEffReco);
-    graph->SetMarkerStyle(7);
-    graph->Draw("AP");
+    TGraph * graphReco= new TGraph(nEntries, ArrInvCut,ArrEffReco);
+    graphReco->SetMarkerStyle(7);
+    graphReco->GetXaxis()->SetTitle("1/TanLambaCut");
+    graphReco->GetYaxis()->SetTitle("RecoMCValidated/Generated");
+    graphReco->SetTitle("Reconstructed Monte Carlo validated tracklets");
+    graphReco->Draw("AP");
 
-    TGraph * graphMean= new TGraph(NumDistinctCuts,DistinctCuts.data(),MeanEffRecoCut.data());
-    graphMean->SetMarkerStyle(20);
-    graphMean-> SetMarkerColor(2);
-    graph->GetXaxis()->SetTitle("1/TanLambaCut");
-    graph->GetYaxis()->SetTitle("RecoMCValidated/Generated");
+    TGraph * graphMeanReco= new TGraph(NumDistinctCuts,DistinctCuts.data(),MeanEffRecoCut.data());
+    graphMeanReco->SetMarkerStyle(20);
+    graphMeanReco-> SetMarkerColor(2);
+    graphMeanReco->Draw("P");
 
-    graphMean->Draw("P");
+
+    TCanvas * c2=new TCanvas("c2","Fake tracklets",1000,700);
+
+    c2->SetLogx();
+    c2->SetLogy();
+
+    TGraph * graphFake= new TGraph(nEntries, ArrInvCut,ArrEffFake);
+    graphFake->SetMarkerStyle(7);
+    graphFake->GetXaxis()->SetTitle("1/TanLambaCut");
+    graphFake->GetYaxis()->SetTitle("Fake Tracklets/Generated");
+    graphFake->SetTitle("Fake tracklets");
+    graphFake->Draw("AP");
+
+    TGraph * graphMeanFake= new TGraph(NumDistinctCuts,DistinctCuts.data(),MeanEffFakeCut.data());
+    graphMeanFake->SetMarkerStyle(20);
+    graphMeanFake-> SetMarkerColor(4);
+    graphMeanFake->Draw("P");
+
+
+
+
 
     //std::cout<<"to draw : "<<NumDistinctCuts<<" "<<*DistinctCuts.data()<<"   "<<*MeanEffRecoCut.data()<<std::endl;
 }
