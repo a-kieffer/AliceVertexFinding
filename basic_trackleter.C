0ca87eb67bdf09481b72ba0fb276776b0e72c048
a-kieffer <akieffer@orange.fr>
2019-07-05 16:40:40 +0200

    macro to plot phi efficiency and macro for basic algorithm validation

---
+++ basic_trackleter.C
@@ -0,0 +1,142 @@
+#if !defined(__CLING__) || defined(__ROOTCLING__)
+
+#include "iostream"
+#include "TChain.h"
+#include "TFile.h"
+#include "TSystem.h"
+#include <TNtuple.h>
+#include <TObject.h>
+
+#include "DataFormatsITSMFT/Cluster.h"
+#include "DataFormatsITSMFT/ROFRecord.h"
+#include "DataFormatsParameters/GRPObject.h"
+#include "SimulationDataFormat/MCEventHeader.h"
+#include "SimulationDataFormat/MCTruthContainer.h"
+
+#include "ITSBase/GeometryTGeo.h"
+#include "ITStracking/IOUtils.h"
+#include "ITStracking/Vertexer.h"
+
+
+using Vertex = o2::dataformats::Vertex<o2::dataformats::TimeStamp<int>>;
+
+//the aim of this macro is to compute tracklets using a tanLambdaCut value and then writes the number of real trackets found, of fakes tracklets 
+//and of the cut value in a file 
+
+
+
+void basic_trackleter(
+                             const int inspEvt = -1,
+                             const int numEvents = 1,
+                             const std::string inputClustersITS = "o2clus_its.root",
+                             const std::string inputGRP = "o2sim_grp.root",
+                             const std::string simfilename = "o2sim.root",
+                             const std::string paramfilename = "O2geometry.root",
+                             const std::string path = "./")
+{
+
+  
+  const auto grp = o2::parameters::GRPObject::loadFrom(path + inputGRP);
+  const bool isITS = grp->isDetReadOut(o2::detectors::DetID::ITS);
+  const bool isContITS = grp->isDetContinuousReadOut(o2::detectors::DetID::ITS);
+  std::cout << "ITS is in " << (isContITS ? "CONTINUOS" : "TRIGGERED") << " readout mode" << std::endl;
+  TChain itsClusters("o2sim");
+  itsClusters.AddFile((path + inputClustersITS).data());
+
+  // Setup Runtime DB
+  TFile paramFile((path + paramfilename).data());
+  paramFile.Get("FAIRGeom");
+  auto gman = o2::its::GeometryTGeo::Instance();
+  gman->fillMatrixCache(o2::utils::bit2Mask(o2::TransformType::T2L, o2::TransformType::T2GRot,
+                                            o2::TransformType::L2G)); // request cached transforms
+
+  // Get event header
+  TChain mcHeaderTree("o2sim");
+  mcHeaderTree.AddFile((path + simfilename).data());
+  o2::dataformats::MCEventHeader* mcHeader = nullptr;
+  if (!mcHeaderTree.GetBranch("MCEventHeader.")) {
+    LOG(FATAL) << "Did not find MC event header in the input header file." << FairLogger::endl;
+  }
+  mcHeaderTree.SetBranchAddress("MCEventHeader.", &mcHeader);
+
+  // get clusters
+  std::vector<o2::itsmft::Cluster>* clusters = nullptr;
+  itsClusters.SetBranchAddress("ITSCluster", &clusters);
+
+  TChain itsClustersROF("ITSClustersROF");
+  itsClustersROF.AddFile((path + inputClustersITS).data());
+
+  if (!itsClustersROF.GetBranch("ITSClustersROF")) {
+    LOG(FATAL) << "Did not find ITS clusters branch ITSClustersROF in the input tree" << FairLogger::endl;
+  }
+  std::vector<o2::itsmft::ROFRecord>* rofs = nullptr;
+  itsClustersROF.SetBranchAddress("ITSClustersROF", &rofs);
+  itsClustersROF.GetEntry(0);
+
+  //get labels
+  o2::dataformats::MCTruthContainer<o2::MCCompLabel>* labels = nullptr;
+  itsClusters.SetBranchAddress("ITSClusterMCTruth", &labels);
+
+
+  std::uint32_t roFrame = 0;
+  const int stopAt = (inspEvt == -1) ? rofs->size() : inspEvt + numEvents;
+  o2::its::ROframe frame(-123);
+  o2::its::VertexerTraits* traits = nullptr;
+  traits = o2::its::createVertexerTraits();
+ 
+ 
+  o2::its::Vertexer vertexer(traits);
+  int EntryNum = (inspEvt ==-1) ? 0 : inspEvt;
+
+  
+
+  for (int iRof = (inspEvt == -1) ? 0 : inspEvt; iRof < stopAt; ++iRof) {
+
+
+    auto rof = (*rofs)[iRof];
+
+    std::cout << "Entry: " << EntryNum << std::endl;
+    EntryNum++;
+    
+    itsClusters.GetEntry(rof.getROFEntry().getEvent());
+    mcHeaderTree.GetEntry(rof.getROFEntry().getEvent());
+    int nclUsed = o2::its::IOUtils::loadROFrameData(rof, frame, clusters, labels);
+    vertexer.initialiseVertexer(&frame);
+
+    vertexer.findTrivialMCTracklets();
+    int T01size = vertexer.getTracklets01().size();
+    int T12size = vertexer.getTracklets12().size();
+    int Linesize = vertexer.getLines().size();
+
+    std::cout<<"Tracklets 01 : "<<T01size<<"  Tracklets 12: "<<T12size<<"  Lines:" <<Linesize<<std::endl;
+
+/* 
+    vertexer.initialiseVertexer(&frame);
+    vertexer.findTracklets(true); 
+    
+    TGenerated += vertexer.getLines().size();
+
+    vertexer.initialiseVertexer(&frame);
+
+    vertexer.findTracklets(false);
+    FakeTracklets += vertexer.getLines().size(); //only the false ones 
+   
+    std::cout<<"Fake tracklets :"<< FakeTracklets<<std::endl;
+
+
+
+    std::cout<<"Tracklets selected :"<<RecoMCvalidated<<"   Total real tracklets :"<<TGenerated<<"  Cut :"<<tanLambdaCut<<std::endl;
+*/
+  }
+
+}
+
+
+
+
+
+#endif
+
+
+
+    
---
+++ plot_efficiency_phi_cut.C
@@ -0,0 +1,293 @@
+#if !defined(__CLING__) || defined(__ROOTCLING__)
+
+#include "iostream"
+#include "TChain.h"
+#include "TFile.h"
+#include "TSystem.h"
+#include <TNtuple.h>
+#include <TLeaf.h>
+#include <TGraph.h>
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TStyle.h"
+
+
+#include "DataFormatsITSMFT/Cluster.h"
+#include "DataFormatsITSMFT/ROFRecord.h"
+#include "DataFormatsParameters/GRPObject.h"
+#include "SimulationDataFormat/MCEventHeader.h"
+#include "SimulationDataFormat/MCTruthContainer.h"
+
+#include "ITSBase/GeometryTGeo.h"
+#include "ITStracking/IOUtils.h"
+#include "ITStracking/Vertexer.h"
+
+#include "ITStracking/ClusterLines.h"
+#include "ITStracking/Tracklet.h"
+#include "ITStracking/Cluster.h"
+
+using Vertex = o2::dataformats::Vertex<o2::dataformats::TimeStamp<int>>;
+
+//the aim of this macro is to read data from root files containing the reconstructed tracklets and compare them to the real tracklets to compute an efficiency and 
+// plot this efficiency with respect to the delta tan lambda cut parameter
+
+void plot_efficiency_phi_cut();
+void plot_quantiles(int nEntries, double * ArrInvCut, double *ArrEff,int * ArrNum, std::vector <int> CountCurrentInvCut,std::vector<double> DistinctCuts);
+void connect_points(double *ArrEff, int * ArrNum, int Num, int NbCuts, std::vector <int>  CountCurrentInvCut, std::vector <double> DistinctCuts,
+    int * FirstCutArrNum, int FirstCutEntries, int offset);
+
+
+
+
+void plot_efficiency_phi_cut(){
+
+    std::string file = "phi_cut_variation.root";
+ 
+    //getting the data from the file
+    TFile *f = new TFile(file.data());
+    TNtuple * Tracklets01 ;
+    TNtuple * Tracklets12 ;
+    f->GetObject("Tracklets01", Tracklets01);
+    f->GetObject("Tracklets12", Tracklets12);
+
+
+    int nEntries=Tracklets01->GetEntries();
+
+
+    TLeaf *leafReco01 = Tracklets01->GetLeaf("RecoMCvalid01");
+    TLeaf *leafTot01 = Tracklets01->GetLeaf("TotTracklets01");
+    TLeaf *leafReco12 = Tracklets12->GetLeaf("RecoMCvalid12");
+    TLeaf *leafTot12 = Tracklets12->GetLeaf("TotTracklets12");
+    TLeaf *leafCut = Tracklets01->GetLeaf("Cut");
+    TLeaf *leafNum = Tracklets01->GetLeaf("EntryNum01");
+    
+    double ArrEffReco01[nEntries];
+    double ArrEffReco12[nEntries];
+    double ArrInvCut[nEntries];
+    int ArrNum[nEntries];
+    
+    std::vector <int> alert01;
+    std::vector <int> alert12;
+
+	for (int i{0}; i < nEntries; ++i){
+        Tracklets01->GetEntry(i);
+        double RecoMCvalidated01 = leafReco01->GetValue(); 
+        double TGenerated01 = leafTot01->GetValue(); 
+        double Cut = leafCut->GetValue(); 
+        double EntryNumber = leafNum->GetValue();
+
+        Tracklets12->GetEntry(i);
+        double RecoMCvalidated12 = leafReco12->GetValue(); 
+        double TGenerated12 = leafTot12->GetValue(); 
+
+        double EffReco01=RecoMCvalidated01/TGenerated01;
+        double EffReco12=RecoMCvalidated12/TGenerated12;
+
+        if(EffReco01>1){
+                alert01.push_back(i);
+            }
+        if(EffReco12>1){
+                alert12.push_back(i);
+            }
+
+
+        ArrEffReco01[i]=EffReco01;
+        ArrEffReco12[i]=EffReco12;
+        ArrInvCut[i]=1/Cut;
+        ArrNum[i]=EntryNumber;
+	}
+
+
+  //sorting the vectors to plot a curve later : sorting according to the Cut value
+    
+
+    std::vector<std::size_t> index_vec;
+    for (std::size_t i = 0; i < (unsigned int)nEntries; ++i) { index_vec.push_back(i); }
+    std::stable_sort( index_vec.begin(), index_vec.end(), [&](std::size_t a, std::size_t b) { return ArrInvCut[a] < ArrInvCut[b]; });
+
+    double SortedArrEffReco01 [nEntries];
+    double SortedArrEffReco12 [nEntries];
+    double SortedArrInvCut [nEntries];
+    int SortedArrNum [nEntries];
+
+    for (std::size_t i = 0; i < (unsigned int) nEntries; ++i)
+    {
+        SortedArrEffReco01[i]=ArrEffReco01[index_vec[i]];
+        SortedArrEffReco12[i]=ArrEffReco12[index_vec[i]];
+        SortedArrInvCut[i]=ArrInvCut[index_vec[i]];
+        SortedArrNum[i]=ArrNum[index_vec[i]];
+    }
+
+    // Computing the mean for each  Cut value to plot it 
+
+    int NumDistinctCuts=0;
+    double CurrentMeanEffReco01=0;
+    double CurrentMeanEffReco12=0;
+    double CurrentInvCut;
+
+    std::vector <int> CountCurrentInvCut; //contains the number of entries for each cut value
+    std::vector<double> DistinctCuts; //contains the distinct cuts, not sorted
+    std::vector<double> MeanEffReco01Cut; //contains the mean EffReco for each cut
+    std::vector<double> MeanEffReco12Cut; 
+    double meanEffReco01;
+    double meanEffReco12;
+
+
+ 
+    for(int i=0; i<nEntries; i++){
+        if( std::find(DistinctCuts.begin(), DistinctCuts.end(), SortedArrInvCut[i])==DistinctCuts.end() || i==0){ //we have not processed this value yet
+
+            CountCurrentInvCut.push_back(0); //always use push back for the 1st element of the vector
+            CurrentInvCut=SortedArrInvCut[i];
+            DistinctCuts.push_back(CurrentInvCut);
+            CurrentMeanEffReco01=0;
+            CurrentMeanEffReco12=0;
+            for(int j=i; j<nEntries; j++){ //all the entries after this one
+                if(SortedArrInvCut[j]==CurrentInvCut){ //if the cut value is the same
+                    CurrentMeanEffReco01+=SortedArrEffReco01[j];
+                    CurrentMeanEffReco12+=SortedArrEffReco12[j];
+                    CountCurrentInvCut[NumDistinctCuts]++; 
+                }
+            }
+            meanEffReco01 = CurrentMeanEffReco01/CountCurrentInvCut[NumDistinctCuts];
+            meanEffReco12 = CurrentMeanEffReco12/CountCurrentInvCut[NumDistinctCuts];
+            MeanEffReco01Cut.push_back(meanEffReco01);
+            MeanEffReco12Cut.push_back(meanEffReco12);
+            NumDistinctCuts++; 
+            std::cout<<"Cut :"<<1/CurrentInvCut<<"     Efficiency 01 :"<<meanEffReco01<<"      Efficiency 12 :"<<meanEffReco12<<std::endl;
+        }else{ //we have already processed this cut
+            continue;
+        }  
+    }
+
+    
+   
+    for (int c : CountCurrentInvCut){
+        std::cout<<"Number of Entries :"<<c<<std::endl;
+    }
+    
+     
+    TCanvas * c1=new TCanvas("c1","Efficiency Reconstruction",1000,700);
+
+
+    c1->Divide(2,1);
+    c1->cd(1)->SetLogx();
+
+    TGraph * graphReco01= new TGraph(nEntries, ArrInvCut,ArrEffReco01);
+    graphReco01->SetMarkerStyle(7);
+    graphReco01->GetXaxis()->SetTitle("1/PhiCut");
+    graphReco01->GetYaxis()->SetTitle("RecoMCValidated01/Generated");
+    graphReco01->SetTitle("Reconstructed Monte Carlo validated tracklets 01");
+    graphReco01->Draw("AP");
+
+    TGraph * graphMeanReco01= new TGraph(NumDistinctCuts,DistinctCuts.data(),MeanEffReco01Cut.data());
+    graphMeanReco01->SetMarkerStyle(20);
+    graphMeanReco01-> SetMarkerColor(2);
+    graphMeanReco01->Draw("PC");
+
+    plot_quantiles(nEntries, SortedArrInvCut, SortedArrEffReco01,  SortedArrNum, CountCurrentInvCut,DistinctCuts);
+
+    c1->cd(2)->SetLogx();
+    
+    TGraph * graphReco12= new TGraph(nEntries, ArrInvCut,ArrEffReco12);
+    graphReco12->SetMarkerStyle(7);
+    graphReco12->GetXaxis()->SetTitle("1/PhiCut");
+    graphReco12->GetYaxis()->SetTitle("RecoMCValidated12/Generated");
+    graphReco12->SetTitle("Reconstructed Monte Carlo validated tracklets 12");
+    graphReco12->Draw("AP");
+
+    TGraph * graphMeanReco12= new TGraph(NumDistinctCuts,DistinctCuts.data(),MeanEffReco12Cut.data());
+    graphMeanReco12->SetMarkerStyle(20);
+    graphMeanReco12-> SetMarkerColor(2);
+    graphMeanReco12->Draw("PC");
+
+    plot_quantiles(nEntries, SortedArrInvCut, SortedArrEffReco12,  SortedArrNum, CountCurrentInvCut,DistinctCuts);
+
+
+    std::cout<<" Number of the entries for 01 : "<<alert01.size()<<std::endl;
+    for (int entry : alert01){
+        std::cout<<entry<<std::endl;
+    }
+
+    std::cout<<" Number of the entries for 12 : "<<alert12.size()<<std::endl;
+    for (int entry : alert12){
+        std::cout<<entry<<std::endl;
+    }
+
+
+}
+ 
+
+
+
+    void plot_quantiles(int nEntries, double * ArrInvCut, double *ArrEff, int * ArrNum, std::vector <int> CountCurrentInvCut,
+     std::vector<double> DistinctCuts){
+
+        // the goal is to find the Number of all the quantiles for the 1st cut value and to then get all the values for this number
+        // an easy way is to sort the efficiency array for the 1st cut value
+
+        int FirstCutEntries = CountCurrentInvCut[0];
+        int offset = FirstCutEntries/5;
+
+        double FirstCutArrEff [FirstCutEntries ];
+        int FirstCutArrNum [FirstCutEntries ];
+        
+
+        std::vector<std::size_t> index_vec;
+        for (std::size_t i = 0; i < (unsigned int) FirstCutEntries ; ++i) { index_vec.push_back(i); }
+        std::sort( index_vec.begin(), index_vec.end(), [&](std::size_t a, std::size_t b) { return ArrEff[a] < ArrEff[b]; });
+        for (std::size_t i = 0; i < (unsigned int) FirstCutEntries ; ++i){
+            FirstCutArrEff[i]=ArrEff[index_vec[i]];
+            FirstCutArrNum[i]=ArrNum[index_vec[i]];
+         }
+
+        int Num1= FirstCutArrNum[0]; //Number of the entry with the lowset Efficiency with the first lambda cut value
+        int Num5= FirstCutArrNum[FirstCutEntries-1-2];
+        int Num2= FirstCutArrNum[offset] ;
+        int Num3= FirstCutArrNum[2*offset] ;
+        int Num4 = FirstCutArrNum[FirstCutEntries-1-offset];
+
+        int NbCuts = DistinctCuts.size();
+     
+        std::sort(DistinctCuts.begin(), DistinctCuts.end()); //we sort it for the plots later
+
+        connect_points(ArrEff, ArrNum, Num1,  NbCuts, CountCurrentInvCut,DistinctCuts, FirstCutArrNum, FirstCutEntries, offset);
+        connect_points(ArrEff, ArrNum, Num2,  NbCuts, CountCurrentInvCut,DistinctCuts, FirstCutArrNum, FirstCutEntries, offset);
+        connect_points(ArrEff, ArrNum, Num3,  NbCuts, CountCurrentInvCut,DistinctCuts, FirstCutArrNum, FirstCutEntries, offset);
+        connect_points(ArrEff, ArrNum, Num4,  NbCuts, CountCurrentInvCut,DistinctCuts, FirstCutArrNum, FirstCutEntries, offset);
+        connect_points(ArrEff, ArrNum, Num5,  NbCuts, CountCurrentInvCut,DistinctCuts, FirstCutArrNum, FirstCutEntries, offset);
+    }
+    
+
+    void connect_points(double *ArrEff, int * ArrNum, int Num, int NbCuts, std::vector <int>  CountCurrentInvCut, std::vector <double> DistinctCuts,
+    int * FirstCutArrNum, int FirstCutEntries, int offset){
+        std::vector <double> Vector;
+        int jOffset=0;
+        std::vector <double> DistinctCutsPlot;
+        
+        //std::cout<<"vector size :"<<Vector.size()<<std::endl;
+
+    
+
+            for(int i=0; i<NbCuts; i++){
+                for(int j=jOffset; j<jOffset+CountCurrentInvCut[i]; j++){ //we only look at one tan lambda cut value
+                    if(ArrNum[j]==Num){ //this is the entry related to this number
+                        Vector.push_back(ArrEff[j]);
+                        DistinctCutsPlot.push_back(DistinctCuts[i]);
+                        std::cout<<"To plot :   Cut :"<<DistinctCuts[i]<<"  Eff "<<ArrEff[j]<<"     Entry Num : "<<Num<<std::endl;
+                        break;
+                    } 
+
+                    
+                }
+                jOffset+=CountCurrentInvCut[i];
+            }
+
+
+        TGraph * graph1= new TGraph(Vector.size(), DistinctCutsPlot.data(),Vector.data());
+        graph1->Draw("PC");
+        
+    }
+    
+    
+#endif
